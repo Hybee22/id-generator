2fa8d3fbda56f131ff9599a1d38032c618ba779d
Ibrahim Adekunle <adefemi101@gmail.com>
2022-05-12 18:14:22 +0100

    updated controller to handle some edge cases

--- controllers/generate-id/index.js
+++ controllers/generate-id/index.js
@@ -1,10 +1,24 @@
 import generatorService from "../../services/generator-service.js";
 import { errorResMsg, successResMsg } from "../../utilities/response.js";
 
+const isDate = (date) => {
+  return new Date(date) !== "Invalid Date" && !isNaN(new Date(date));
+};
+
+const currentDate = new Date();
+const timestamp = currentDate.getTime();
 class SequenceGeneratorController {
   async generate(req, res) {
     try {
       const { start, machineId } = req.body;
+
+      const validDate = isDate(start);
+      if (!validDate) return errorResMsg(res, 400, { message: "Invalid Date" });
+
+      if (+new Date(start) > timestamp)
+        return errorResMsg(res, 400, {
+          message: "Epoch must be less than current date",
+        });
 
       const generatedId = await generatorService.generate({
         start,
